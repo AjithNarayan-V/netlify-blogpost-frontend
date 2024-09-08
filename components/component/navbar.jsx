ac97e053bd09643548543beb6c4dd0377ba20443
AjithNarayan-V <ajithnarayan004@gmail.com>
2024-09-08 19:32:10 +0530

    nav update

--- components/component/navbar.jsx
+++ components/component/navbar.jsx
@@ -27,7 +27,7 @@ export function Navbar() {
             <span className="sr-only">Acme Inc</span>
           </Link>
           <div className="grid gap-2 py-6">
-           
+          
             <Link
               href="/"
               className="flex w-full items-center py-2 text-lg font-semibold"
@@ -41,17 +41,14 @@ export function Navbar() {
               Create Post
             </Link>
             <Link
-              href="/viewPost"
+              href="/"
               className="flex w-full items-center py-2 text-lg font-semibold"
               prefetch={false}>
               View Post
             </Link>
+            
             <Link
-              href="#"
-              className="flex w-full items-center py-2 text-lg font-semibold"
-              prefetch={false}>Explore Blogs</Link>
-            <Link
-              href="#"
+              href="/contact"
               className="flex w-full items-center py-2 text-lg font-semibold"
               prefetch={false}>
               Contact
@@ -77,8 +74,6 @@ export function Navbar() {
           prefetch={false}>
           Create Post
         </Link>
-       
-        
         <Link
           href="/contact"
           className="group inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus:bg-gray-800 dark:focus:text-gray-50 dark:data-[active]:bg-gray-800/50 dark:data-[state=open]:bg-gray-800/50"
@@ -95,6 +90,7 @@ export default Navbar;
 
 
  function MenuIcon(props) {
+
   return (
     <Image
       {...props}
